934ddb8ff6cfea52a731f46507e9472fc050e10a
Henrico Ferdian <henrico.ferdian@gopay.co.id>
2025-08-04 14:27:22 +0700

    refactor: rename variables for accessors

--- newrelic.go
+++ newrelic.go
@@ -4,10 +4,10 @@ import (
 	"github.com/newrelic/go-agent"
 )
 
-func getNewRelicConfigOrPanic(loader *ConfigAccessor) newrelic.Config {
-	config := newrelic.NewConfig(getStringOrPanic(loader, "NEW_RELIC_APP_NAME"), getStringOrPanic(loader, "NEW_RELIC_LICENCE_KEY"))
-	config.Enabled = getFeature(loader, "NEW_RELIC_ENABLED")
-	labels, err := parseNewRelicLabels(getStringOrPanic(loader, "NEW_RELIC_LABELS"))
+func getNewRelicConfigOrPanic(accessor *ConfigAccessor) newrelic.Config {
+	config := newrelic.NewConfig(getStringOrPanic(accessor, "NEW_RELIC_APP_NAME"), getStringOrPanic(accessor, "NEW_RELIC_LICENCE_KEY"))
+	config.Enabled = getFeature(accessor, "NEW_RELIC_ENABLED")
+	labels, err := parseNewRelicLabels(getStringOrPanic(accessor, "NEW_RELIC_LABELS"))
 	if err == nil {
 		config.Labels = labels
 	}
--- utils.go
+++ utils.go
@@ -5,24 +5,24 @@ import (
 	"github.com/spf13/cast"
 )
 
-func getIntOrPanic(loader *ConfigAccessor, key string) int {
-	v, ok := loader.Get(key)
+func getIntOrPanic(accessor *ConfigAccessor, key string) int {
+	v, ok := accessor.Get(key)
 	if !ok {
 		panic(fmt.Errorf("%s key is not set", key))
 	}
 	return cast.ToInt(v)
 }
 
-func getFeature(loader *ConfigAccessor, key string) bool {
-	v, ok := loader.Get(key)
+func getFeature(accessor *ConfigAccessor, key string) bool {
+	v, ok := accessor.Get(key)
 	if !ok {
 		return false
 	}
 	return cast.ToBool(v)
 }
 
-func getStringOrPanic(loader *ConfigAccessor, key string) string {
-	v, ok := loader.Get(key)
+func getStringOrPanic(accessor *ConfigAccessor, key string) string {
+	v, ok := accessor.Get(key)
 	if !ok {
 		panic(fmt.Errorf("%s key is not set", key))
 	}
